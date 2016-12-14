7ca5a9285106fd0825f6a4331e91c2431c48f81f
crystalxian <dzx07522@163.com>
2016-12-14 10:47:15 +0800

    Update 2016-12-12-speech-recognition.m

--- _posts/2016-12-12-speech-recognition.m
+++ _posts/2016-12-12-speech-recognition.m
@@ -6,4 +6,21 @@ categories: ASR
 tags: ASR
 ---
 
+* content
+{:toc}
+
+
 语音是由调节动态系统中相对少的参数来产生的。这意味着隐藏在语音特征下的真正结构的复杂度，比直接描述现有特征的模型要小得多。
+
+## 简介ASR
+ASR(Automatic speech recognition)　speech-to-text transcription
+
+### ASR实现难点
+* Variability in speech recognition
+
+1. Size Number of word types in vocabulary, perplexity
+2. Speaker Tuned for a particular speaker, or speaker-independent? Adaptation to speaker characteristics and accent
+3. Acoustic environment Noise, competing speakers, channel conditions (microphone, phone line, room acoustics)
+4. Style Continuously spoken or isolated? Planned monologue or spontaneous conversation?
+
+* 
