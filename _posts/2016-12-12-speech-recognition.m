17dd0227d5b49f0a5a5adbfb37773ecdf342adf3
crystalxian <dzx07522@163.com>
2016-12-14 10:59:41 +0800

    Update 2016-12-12-speech-recognition.m

--- _posts/2016-12-12-speech-recognition.m
+++ _posts/2016-12-12-speech-recognition.m
@@ -1,17 +1,13 @@
 ---
 layout: post
 title:  "hello ASR!"
-date:   2016-12-12
+date:  2016-12-12
 categories: ASR
 tags: ASR
 ---
 
-  
-  
-  
 * content
 {:toc}
-
 
 语音是由调节动态系统中相对少的参数来产生的。这意味着隐藏在语音特征下的真正结构的复杂度，比直接描述现有特征的模型要小得多。
 
